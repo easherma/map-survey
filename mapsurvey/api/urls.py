10c5fd8916c88b801405c682910f8c76aa784a5a
Eric Sherman <ericandrewsherman@gmail.com>
2017-05-11 22:31:00 -0500

    clean up bad urls

--- mapsurvey/api/urls.py
+++ mapsurvey/api/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from api import views
-from djgeojson.views import GeoJSONLayerView
-from geopolls.models import Submission
+
 
 router = routers.DefaultRouter()
 router.register(r'submissions', views.SubmissionViewSet)
+router.register(r'data', views.LocationViewSet)
 router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^data', GeoJSONLayerView.as_view(model=Submission), name='data'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
