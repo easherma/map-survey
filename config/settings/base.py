7ce33aabdb6d41e9f6b5c064d54a8c165a35a0c3
Eric Sherman <ericandrewsherman@gmail.com>
2017-05-15 18:33:46 -0500

    allow all

--- config/settings/base.py
+++ config/settings/base.py
@@ -302,11 +302,13 @@ LEAFLET_CONFIG = {
 }
 
 #cors config
-CORS_ORIGIN_WHITELIST = (
-    'google.com',
-    'hostname.example.com',
-    'localhost:8000',
-    'localhost:4000',
-    '127.0.0.1:8000',
-    '127.0.0.1:4000'
-)
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     'google.com',
+#     'hostname.example.com',
+#     'localhost:8000',
+#     'localhost:4000',
+#     '127.0.0.1:8000',
+#     '127.0.0.1:4000'
+# )
